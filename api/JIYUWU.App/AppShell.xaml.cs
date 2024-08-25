dc886420ff052619d3f125c6acd25b9bbca348e0
ych <923374829@qq.com>
2024-08-25 16:08:43 +0800

    修改app位置

---
+++ api/JIYUWU.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JIYUWU.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ api/JIYUWU.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JIYUWU.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
