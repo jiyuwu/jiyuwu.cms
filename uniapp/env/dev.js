6a85802545fff397d50689dd0e1d1ec7a0814bd9
ych <923374829@qq.com>
2024-12-01 19:27:43 +0800

    移除相关vue文件

--- uniapp/env/dev.js
+++
@@ -1,5 +0,0 @@
-const ENV_TYPE={
-	baseUrl:"http://localhost:58956"
-}
-
-module.exports = ENV_TYPE;
--- uniapp/env/prod.js
+++
@@ -1,5 +0,0 @@
-const ENV_TYPE={
-	baseUrl:"http://127.0.0.1:8002"
-}
-
-module.exports = ENV_TYPE;
